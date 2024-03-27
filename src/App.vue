5832108480ae37c4eeb25cb3a7a4804ebbee97d0
kim hyung ju <pskhj0812@gmail.com>
2024-03-27 15:23:58 +0900

    라우트 추가

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,11 @@ import TheView from '@/layouts/TheView.vue'
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
+    <div class="link">
+      <router-link to="/">Home</router-link> | 
+      <router-link to="/about">About</router-link> |
+    </div>
+
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
