295b39384fc9291cc318f4427763f2a446bc5a8d
kim hyung ju <pskhj0812@gmail.com>
2024-03-27 14:56:55 +0900

    커밋

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = defineConfig({
-  transpileDependencies: true,
-  filenameHashing: true,
-  configureWebpack: (config) => {
-    config.output.filename = 'js/[name].[hash].js'
-    config.output.chunkFilename = 'js/[name].[hash].js'
-  },
-  publicPath: process.env.NODE_ENV === 'production' ? '/test/vue_test/' : '/',
-})
